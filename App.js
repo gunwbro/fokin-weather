6772559c2d6662f87008551c6e467294e7d13219
Gunwoo Jo <gunwbro@gmail.com>
2019-12-31 22:11:03 +0900

    Displaying Temperature

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import { alert, Alert } from "react-native";
+import axios from "axios";
+
+const API_KEY = "d042d57373998d02f27f6f7d091a9ea3";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
+}
